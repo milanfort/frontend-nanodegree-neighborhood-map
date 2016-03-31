cbe925dd07afc9a13e1f8a90eaaac0a05259a8d9
Milan Fort <milan.fort@gmail.com>
2016-03-31 03:36:47 +0200

    Create gulp js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var project = require('./package.json');
 var del = require('del');
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
@@ -22,6 +23,15 @@ gulp.task('css', function () {
         .pipe(browserSync.stream());
 });
 
+gulp.task('js', function () {
+    return gulp.src(source + '/js/spa.js')
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dest + '/js/'))
+        .pipe(browserSync.stream());
+});
+
 gulp.task('default', function () {
     console.log('Building %s version %s', project.name, project.version);
 
@@ -32,4 +42,5 @@ gulp.task('default', function () {
 
     //gulp.watch(source + '/**/*', browserSync.reload);
     gulp.watch(source + '/css/*.css', ['css']);
+    gulp.watch(source + '/js/**/*.js', ['js']);
 });
