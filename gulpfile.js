57eedc22f60efc30cd3c605fbce801a6b66788b2
Milan Fort <milan.fort@gmail.com>
2016-03-31 02:19:01 +0200

    Integrate browser sync into build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var project = require('./package.json');
 var del = require('del');
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
 var source = 'src';
 var dest = 'dist';
@@ -12,4 +13,11 @@ gulp.task('clean', function (done) {
 
 gulp.task('default', function () {
     console.log('Building %s version %s', project.name, project.version);
+
+    browserSync.init({
+        server: './src',
+        online: false
+    });
+
+    gulp.watch(source + '/**/*', browserSync.reload);
 });
