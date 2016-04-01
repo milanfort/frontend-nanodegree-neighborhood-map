d22afaac2cc988ba724ac5e363f7fe8f18263c6f
Milan Fort <milan.fort@gmail.com>
2016-04-01 17:32:40 +0200

    Integrate chai assertion library

--- test/util-spec.js
+++ test/util-spec.js
@@ -1,23 +1,23 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var util = require('../src/js/util.js');
 
 describe('Util Tests', function () {
     describe('first', function () {
         it('should return the first number from the array', function () {
             var result = util.first([2, 3, 4]);
-            assert.equal(2, result);
+            expect(result).to.equal(2);
         });
     });
 
     describe('capitalize', function () {
         it('should convert first letter to uppercase', function () {
             var result = util.capitalize('foo');
-            assert.equal('Foo', result);
+            expect(result).to.equal('Foo');
         });
 
         it('should do nothing if first character is not a letter', function () {
             var result = util.capitalize('123foo');
-            assert.equal('123foo', result);
+            expect(result).to.equal('123foo');
         });
     });
 });
